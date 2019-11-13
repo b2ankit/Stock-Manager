7b931182048493fe2030e463cb8c87cd278dd7b9
B2Ankit <ankit15ec007@gmail.com>
2019-11-13 11:06:08 +0530

    view completed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Stock Manager' });
 });
 
+router.get('/records',function(req,res,next){
+  res.render('records',{title:'Stock Manager'})
+})
+
+router.get('/Stocks',function(req,res,next){
+  res.render('stocks',{title:'Stock Manager'})
+})
 module.exports = router;
