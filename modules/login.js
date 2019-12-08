d9f0f6495ac442c19a01cef173667ff7ede84960
B2Ankit <ankit15ec007@gmail.com>
2019-12-08 05:44:20 +0530

    url added

--- modules/login.js
+++ modules/login.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 
 bodyparser = require("body-parser");
 
-mongoose.connect('mongodb://localhost:27017/stock',{useNewUrlParser:true,useUnifiedTopology: true})
-
+// mongoose.connect('mongodb://localhost:27017/stock',{useNewUrlParser:true,useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://test:test@cluster0-hszqu.mongodb.net/test?retryWrites=true&w=majority')
 var conn = mongoose.Connection;
 
 var adminLoginSchema = new mongoose.Schema({
